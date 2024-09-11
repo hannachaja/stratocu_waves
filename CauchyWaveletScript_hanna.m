5e1ff3fd08650badfed15a3050575dd44e0f45aa
hannachaja <hxc907@miami.edu>
2024-09-11 13:16:26 -0400

    Update CauchyWaveletScript_hanna.m

--- CauchyWaveletScript_hanna.m
+++ CauchyWaveletScript_hanna.m
@@ -14,11 +14,17 @@ imshow(frame50); colorbar;
 
 %%% Wavelet transform 
 
-% a set of 8 Angles from 0 to pi 
-Angles = 0:pi/8:pi-pi/8
+% a set of 6 Angles from 0 to pi 
+Angles = 0:pi/6:pi-pi/6 ;
+
 
 % a set of 10 Scales
-Scales = 5:5:50
+% Scales = 5:5:50
+
+
+% a LOGARITHMIC set of 10 Scales (1.2)
+% Scales = [2,5,10,20,40,80,160,320,640,1000] ;
+Scales = 10.^(1.2:.2:3) ;
 
 cwtCauchy = cwtft2(frame50,wavelet="cauchy",scales=Scales, angles=Angles);
 
@@ -29,18 +35,20 @@ size(spec)
 
 
 % Overlay wavelet power on image 
-figure(1)
-imshow(frame50); colorbar; axis on
+%figure(1)
+%imshow(frame50); colorbar; axis on
 
-hold on
+%hold on
 
-posLevels = 10:20:90; negLevels = -90:20:-10; 
-powlevels = 1000:1000:5000;
+%posLevels = 10:20:90; negLevels = -90:20:-10; 
+%powlevels = 1000:1000:5000;
+
 
 % Real part is crests and trofs, imag is gradients, abs is a magnitude map 
-contour( real(spec(:,:,10,7)), LevelList=posLevels,EdgeColor='red' );
-contour( real(spec(:,:,10,7)), LevelList=ne ...
-    gLevels,EdgeColor='blue' );
+%contour( real(spec(:,:,10,7)), LevelList=posLevels,EdgeColor='red' );
+%contour( real(spec(:,:,10,7)), LevelList=negLevels,EdgeColor='blue' );
 
 % "power" is amplitude abs() squared 
-contour( abs(spec(:,:,10,7)).^2 );
+%contour( abs(spec(:,:,10,7)).^2 );
+
+hanna_image_with_wavelet_overlay(frame50,spec, Scales, 5, 6)
